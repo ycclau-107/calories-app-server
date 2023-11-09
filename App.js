bdf24da7d52975f31ce2babf767136095c0eff81
kamada01 <ada.kamym@gmail.com>
2023-11-10 01:05:25 +0800

    Added camera permission request, barcode scanning screen

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect }  from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Barcode_scanner } from "./src/Barcode_scanner";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Barcode_scanner/>
   );
+
 }
 
 const styles = StyleSheet.create({
